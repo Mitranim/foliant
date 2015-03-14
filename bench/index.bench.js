4ea89b22f2d86211218f4147a341d66c98acd80c
Mitranim <me@mitranim.com>
2015-03-14 09:50:18 +0300

    cleaned up bench

--- bench/index.bench.js
+++ bench/index.bench.js
@@ -37,48 +37,6 @@ function defCount() {
 }
 
 function mockSourceWords() {
-  return ['three', 'random', 'words']
-}
-
-function mockSounds() {
-  return ['th', 'r', 'e', 'a', 'n', 'd', 'o', 'm', 'w', 's']
-}
-
-function mockVowels() {
-  return ['e', 'a', 'o']
-}
-
-function mockSoundSet() {
-  var set = new Set()
-  for (var sound of mockSounds()) set.add(sound)
-  return set
-}
-
-function mockVowelSet() {
-  var set = new Set()
-  for (var vowel of mockVowels()) set.add(vowel)
-  return set
-}
-
-function mockPairSet() {
-  return new PairSet([
-    new Pair('th', 'r'),
-    new Pair('r', 'e'),
-    new Pair('e', 'e'),
-    new Pair('r', 'a'),
-    new Pair('a', 'n'),
-    new Pair('n', 'd'),
-    new Pair('d', 'o'),
-    new Pair('o', 'm'),
-    new Pair('w', 'o'),
-    new Pair('o', 'r'),
-    new Pair('r', 'd'),
-    new Pair('d', 's')
-  ])
-}
-
-function mockTraits() {
-  var traits = new Traits()
-  traits.examine(mockSourceWords())
-  return traits
+  // return ['three', 'random', 'words']
+ return ['go', 'nebula', 'aurora', 'theron', 'thorax', 'deity', 'quasar']
 }
